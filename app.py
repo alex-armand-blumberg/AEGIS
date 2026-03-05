a9a82a80bd61f61691360d5c02792f7c960977c9
Alex Armand-Blumberg <alex.armandblumberg@gmail.com>
2026-03-05 14:59:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ override_map_dates = st.sidebar.checkbox(
 col1, col2 = st.columns([1, 12])
 
 with col1:
-    st.image("logo.png", width=60)
+    st.image("logo.png", width=1000)
 
 with col2:
     st.title("AEGIS — Escalation Detection Demo")
