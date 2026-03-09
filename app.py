94c157dade36defabe7787bee47e836ccf7a2a10
Alex Armand-Blumberg <alex.armandblumberg@gmail.com>
2026-03-08 20:13:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -929,7 +929,7 @@ if "aegis_plot" in st.session_state:
             f"<br>"
             f"Battles: <b>{int(row.get('battles',0)):,}</b><br>"
             f"Explosions: <b>{int(row.get('explosions_remote_violence',0)):,}</b><br>"
-            f"🎯 Strategic devs: <b>{int(row.get('strategic_developments',0)):,}</b><br>"
+            f"Strategic devs: <b>{int(row.get('strategic_developments',0)):,}</b><br>"
             f"✊ Protests: <b>{int(row.get('protests',0)):,}</b><br>"
             f"🔥 Riots: <b>{int(row.get('riots',0)):,}</b><br>"
             f"👥 Civ. violence: <b>{int(row.get('violence_against_civilians',0)):,}</b><br>"
